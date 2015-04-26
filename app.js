d41de6ed6e7cadd20d1c9829d230b9b632821917
Jonathan Lin <johnlin0214@gmail.com>
2015-04-25 20:37:06 -0400

    Add partials folder and nyt and home view

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var myApp = angular.module('myApp', [])
+var myApp = angular.module('myApp', ['ui-router'])
+
+myApp.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html",
+
+  });
+  $stateProvider.state('nyt', {
+    url: '/yt',
+    templateUrl: "partials/nyt.html"
+    controller: "newsCtrl"
+  });
+})
