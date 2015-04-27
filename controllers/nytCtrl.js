0f446c72a107ebcd2ff6d478c942ed22ea84e2c7
Jonathan Lin <johnlin0214@gmail.com>
2015-04-27 10:13:22 -0400

    Disable voting and comments for nyt

--- controllers/nytCtrl.js
+++ controllers/nytCtrl.js
@@ -1,16 +1,10 @@
 myApp.controller('nytCtrl', function nytCtrl($scope, $http, $interval, nytFactory) {
     window.callbackTopStories = function(data) {
     $scope.nytPrograms = data.results;
-    for (i = 0; i < $scope.nytPrograms.length; i++) {
-        $scope.nytPrograms[i].vote = 0;
-        $scope.nytPrograms[i].comments = [];
-    }
     }
     nytFactory.fetch().then(function(data) {
         $scope.nytPrograms = data.results
-
     });
-
     $scope.addComment = function(item,comment) {
         item.comments.push(comment);
     }
