5689e540f9ea4f18ef237ea24ed4381142abca60
jonlin <johnlin0214@gmail.com>
2016-03-31 10:34:27 -0700

    Remove use of scope form npr controller

--- controllers/nprCtrl.js
+++ controllers/nprCtrl.js
@@ -1,20 +1,23 @@
-myApp.controller('nprCtrl', ['$scope', '$http', '$interval', 'nprFactory', function($scope, $http, $interval, nprFactory) {
+myApp.controller('nprCtrl', ['$http', '$interval', 'nprFactory', function($http, $interval, nprFactory) {
+		var vm = this;
     nprFactory.fetch().then(function(data) {
-        $scope.nprPrograms = data.list.story;
-        for (i = 0; i < $scope.nprPrograms.length; i++) {
-            $scope.nprPrograms[i].vote = 0;
-            $scope.nprPrograms[i].comments = [];
+        vm.nprPrograms = data.list.story;
+        for (i = 0; i < vm.nprPrograms.length; i++) {
+            vm.nprPrograms[i].vote = 0;
+            vm.nprPrograms[i].comments = [];
         }
 
     });
-    $scope.addComment = function(item,comment) {
+
+    vm.addComment = function(item,comment) {
         item.comments.push(comment);
     }
-    $scope.addValue = function(item) {
+    vm.addValue = function(item) {
         item.vote += 1;
     }
-    $scope.lowerValue = function(item) {
+    vm.lowerValue = function(item) {
         item.vote -= 1;
     }
-
+		vm.name = 'John';
+		console.log(vm);
 }]);
