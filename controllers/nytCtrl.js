7fc7345a2c3e8149faf332081f95c93f2c032ac9
jonlin <johnlin0214@gmail.com>
2016-03-31 10:39:54 -0700

    Update nyt partial to use controller as syntax

--- controllers/nytCtrl.js
+++ controllers/nytCtrl.js
@@ -1,11 +1,11 @@
 myApp.controller('nytCtrl', ['$http', '$interval', 'nytFactory', function($http, $interval, nytFactory) {
-    window.callbackTopStories = function(data) {
 		var vm = this;
-    vm.nytPrograms = data.results;
-    for (i = 0; i < vm.nytPrograms.length; i++) {
-        vm.nytPrograms[i].vote = 0;
-        vm.nytPrograms[i].comments = [];
-    }
+    window.callbackTopStories = function(data) {
+	    vm.nytPrograms = data.results;
+	    for (i = 0; i < vm.nytPrograms.length; i++) {
+	        vm.nytPrograms[i].vote = 0;
+	        vm.nytPrograms[i].comments = [];
+	    }
     }
     nytFactory.fetch().then(function(data) {
         vm.nytPrograms = data.results
